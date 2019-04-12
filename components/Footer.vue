2e54360c6a5044ebd320c1bd3b0ddd92dfc99e7c
vwgolfg9 <vwgolfg9@gmail.com>
2019-04-12 21:27:52 +0300

    dermalogic

---
+++ components/Footer.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="global columns is-mobile">
+        <div id="logoContainer" class="column is-2">
+            <img id="logo" src="@/assets/dermalogic-logo-2.png" alt="">
+        </div>
+        <div id="oraria" class="column ">
+            <p><strong>Δευτέρα - Σάββατο :<br> 08:00 - 17:00</strong></p>
+        </div>
+        <div id="hexContainer" class="column is desktop">
+            <h1 id="logia">Designed by: <span><img id="hexagon" src="@/assets/logo hexagon.png" width="70" alt=""></span></h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+    .columns{
+        margin: 0;
+        padding:0;
+        
+    }
+    .column{
+        height:100px;
+        margin: 0;
+        padding:0;
+    }
+
+    #logo{
+        position: relative;
+        margin:0;
+        width: 50%;
+        top: 20px;
+        left: 20px;
+    
+    }
+    #oraria{
+        position:relative;
+        top:20px;
+        left: 80px;
+    }
+
+    #hexagon{
+        position: relative;
+        top:5px;
+        margin-top: 20px;
+        margin-left: 10px;
+        padding-bottom: 0px; 
+    }
+
+    #logia{
+        margin:auto;
+    }
+   
+    @media(max-width:768px){
+        #logo{
+            width:100%;
+        }
+        #oraria{
+            left: 70px;
+        }
+        #hexContainer{
+            position: relative;
+            top:80px;
+        }
+        #hexagon{
+        position: relative;
+        top:0px;
+        margin-top: 0px;
+        margin-left: 10px;
+        padding-bottom: 0px; 
+        }
+    }
+</style>
+
---
+++ pages/thankyou/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="global">
+        <app-header></app-header>
+        <div class="hero is-medium is-bold is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 ref="title" class="title is-1">ΕΥΧΑΡΙΣΤΟΥΜΕ</h1>
+                    <h1 ref="subtitle" class="subtitle is-1">Καλή Επιτυχία!</h1>
+                    <a ref="" href="/" class="button is-danger">Επιστροφή στην Αρχική</a>   
+
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>
+</template>
+<script>
+import { TimelineLite } from 'gsap'
+import Header from "@/components/Header.vue"
+export default {
+    components: {
+        appHeader : Header,
+    },
+    mounted() {
+        const {title} = this.$refs;
+        const timeline = new TimelineLite();
+        
+        timeline.to(title, 1, {y: -80, ease: Back.easeInOut})
+
+        const {subtitle} = this.$refs;
+
+        timeline.to(subtitle, 1, {y: -60, ease: Back.easeInOut, }, "-=0.5" )
+
+    }
+}
+</script>
+<style scoped>
+    .hero-body{
+        background: #502eb0;
+    }
+</style>
