4d5f58a4f3d15f199506665e6cc2e65524ca783e
vwgolfg9 <vwgolfg9@gmail.com>
2019-04-16 14:20:04 +0300

    header logo animation

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <div class="global">
     <div id="headerContainer" class="columns is-mobile">
       <div class="column">
-        <a href="/" ><img class="logo" src="@/assets/dermalogic-logo-2.png" width="80" alt=""></a>
+        <a href="/" ><img class="logo" src="@/assets/logo-animation.svg" width="80" alt=""></a>
       </div>
       <div id="socialContainer" class="columns column is-4-mobile is-3-desktop is-mobile container-social ">
         <a href="https://www.facebook.com/dermalogicskin/?ref=search&__tn__=%2Cd%2CP-R&eid=ARA5NRFIIf93j1q1R8JdHhK2uCGBB9oM9E2Cq4AvbOH3czJtZrVPeYeD94U5FIi55VvclwsB0AVcH2lF" 
