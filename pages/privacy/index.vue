72c07ed5c076f499761ffdae3c47bb287a14ab7e
vwgolfg9 <vwgolfg9@gmail.com>
2019-04-18 18:14:12 +0300

    privacy

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -5,7 +5,7 @@
             <div class="hero-body">
                 <div class="container">
                     <div class="title">
-                        mply
+                       
                     </div>
                 </div>
             </div>
@@ -18,24 +18,36 @@
                 <h1 class="is-size-5">Personal Data collected for the following purposes and using the following services:</h1>
                 <div id="purpose" class="columns is-mobile">
                     <div id="analyticsContainer" class="column is-one-fifth">
-                        <img id="analytics"  src="@/assets/analytics.png" alt="">
+                        <img id="analytics" style="margin-left: 9px; margin-top:8px;" width="60" src="@/assets/analytics.svg" alt="">
                     </div>
                     <div class="column">
-                        <h1 is-size-2>Analytics</h1>
-                        <p></p>
+                        <h1 class="is-size-3">Analytics</h1>
+                        <p class="is-size-6">Personal data: Usage data</p>
+                    </div>
+                </div>
+                <div style="margin-top: 10px;" class="contact columns is-mobile">
+                    <div class="column is-one-fifth envelopeContainer">
+                        <img id="envelope" src="@/assets/envelope.svg" style="margin-left: 10px; margin-top:17px;" width="60" alt="">
+                    </div>
+                    <div class="column">
+                        <h1 class="is-size-3" >Contacting the user</h1>
+                        <h1 class="is-size-5 ">Contact form</h1>
+                        <h1 class="is-size-6">Personal Data: email address, first name, last name</h1>
                     </div>
                 </div>
             
             </div>
         </div>
+        <app-footer></app-footer>
     </div>
 </template>
 <script>
 import Header from "@/components/Header.vue"
-
+import Footer from "@/components/Footer.vue"
 export default {
     components: {
         appHeader : Header,
+        appFooter : Footer,
     },
 }
 </script>
@@ -52,16 +64,25 @@ export default {
           
         height:auto;
     }
-    #analytics{
-     width:30%;
-     margin-left:10px;
-    }
+    
     #analyticsContainer{
 
     }
     #purpose{
         margin-top:20px;
 
+    }
+    .envelopeContainer{
+    }
+
+    @media(max-width: 768px){
+        #envelope{
+            width:40px;
+        }
+        #background{
+            width:100%;
+            height:160px;
+        }
     }
 </style>
 
